7c6341a35379204994730c012ce26f09d25d556d
ALI ALRAWI <ali.developer90@gmail.com>
2017-01-10 17:52:38 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,26 @@
 'use strict';
+function fancySchmancy() {
+  return $('#bacon').addClass('iAmFancy');
+}
+function addText(text) {
+  return  $('p:last').val(text)
+}
+function showDolphin() {
+   $('#hidden').show()
+}
+function hideImage() {
+   $('img[alt="pie in face"]').hide(0)
+}
+function fadeIt() {
+   $('#favorite-snacks').fadeIn()
+}
+function fadeItOut() {
+   $('p:last').fadeOut(0)
+}
+
+function findItByClass(tag) {
+  return  $(tag).hasClass("flatironLink")
+}
+function formValue() {
+  return  $('form input:last-child').val()
+}
