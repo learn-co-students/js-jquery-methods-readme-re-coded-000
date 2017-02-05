462b0dba8b5cd4cfc6c9f7284e06821df6445c21
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2017-02-05 16:05:35 +0000

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,26 @@
 'use strict';
+function fancySchmancy() {
+  $('p#bacon').addClass("iAmFancy");
+  return $('.iAmFancy');
+}
+function addText(str) {
+  return $('p:first-child + p').append(str);
+}
+function showDolphin() {
+  $('img#hidden').show()
+}
+function hideImage() {
+  $('img[alt="pie in face"]').hide()
+}
+function fadeIt() {
+  $('div#favoorite-snacks').fadeIn();
+}
+function fadeItOut() {
+  $('p:first-child + p').fadeOut(0);
+}
+function findItByClass(selector) {
+  return $(selector).hasClass("flatironLink");
+}
+function formValue() {
+  return $('input:last').val();
+}
