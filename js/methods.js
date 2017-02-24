82d7d5ce2ec10b3c1224ecb243fb47a32476c0ac
Fatima Berazi <fatimaberaze1997@gmail.com>
2017-02-24 18:22:19 +0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+function fancySchmancy(){
+	$('#bacon').addClass('iAmFancy');
+	return $('.iAmFancy');
+}
+function addText(){
+	$('p:first-child + p')
+  return $('p:first-child + p');
+}
+function showDolphin(){
+	$('#hidden').show();
+}
+function hideImage(){
+	$('img[alt="pie in face"]').hide();
+}
+function fadeIt(){
+  $("#favorite-snacks").fadeIn();
+}
+function fadeItOut(){
+  $('p:first-child + p').fadeOut(0);
+  return $('p:first-child + p');
+}
+
+function findItByClass(selector){
+  return $(selector).hasClass("flatironLink");
+}
+
+function animateIt(){
+  $('#box').animate({height: "500px"});
+}
+
+
+function formValue(){
+  return $('input:last').val();
+  }
