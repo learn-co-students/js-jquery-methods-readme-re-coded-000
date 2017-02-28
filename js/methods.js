8c9a676ffc439816c44f1b9d0632d97de6fd7341
Omar Suliman <omarsulyman@gmail.com>
2017-03-01 01:53:21 +0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,57 @@
 'use strict';
+function fancySchmancy() {
+	 $('#bacon').addClass("iAmFancy")
+	 return $('.iAmFancy')
+}
+
+function addText(sentence){
+  $('p:first-child + p').append(sentence);
+  return $('p:first-child + p');
+}
+
+function showDolphin() {
+	 $('#hidden').show()
+}
+
+function hideImage() {
+	 $('img[alt="pie in face"]').hide()
+}
+
+function fadeIt() {
+	 $("#favorite-snacks").fadeIn()
+}
+
+function fadeItOut() {
+	 $('p:first-child + p').fadeOut(0)
+}
+
+function findItByClass(string) {
+	 return $(string).hasClass("flatironLink")
+}
+
+function formValue() {
+	 return $('input:last').val()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
