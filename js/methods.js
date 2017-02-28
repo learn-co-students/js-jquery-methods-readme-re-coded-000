cf7f2e8bbdc1bec9ba48f9371d0546720725cfe4
Nichervan <nichervan.brhik@gmail.com>
2017-02-28 22:00:06 +0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function fancySchmancy() {
+	$('p').addClass('iAmFancy #bacon')
+	return $('p')
+}
+
+function addText(str) {
+	return $("p:nth-child(2)").append('str')
+}
+
+function showDolphin() {
+	$('img').show();
+}
+
+function hideImage() {
+	$("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+	$('#favorite-snacks').fadeIn()
+}
+
+function fadeItOut() {
+	$("p:nth-child(2)").fadeOut(0)
+}
+
+function findItByClass(str){
+	return $(str).hasClass("flatironLink");
+}
+
+function formValue() {
+	return $('input:last').val();
+}
