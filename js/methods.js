a8c8493624cdd7d35bb9e984bd2fd770da38fac1
Leloz Suliman <leloz997@gmail.com>
2017-03-02 13:57:53 +0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,36 @@
 'use strict';
+function fancySchmancy(){
+	$('#bacon').addClass("iAmFancy");
+	return $('.iAmFancy');
+}
+
+function addText(string){
+	$('p:first-child + p').append(string);
+	return $('p:first-child + p');
+}
+
+function showDolphin(){
+	$("#hidden").show();
+}
+
+function hideImage(){
+	$('img[alt="pie in face"]').hide();
+}
+
+function fadeIt(){
+	$("#favorite-snacks").fadeIn();
+}
+
+function fadeItOut(){
+	$('p:last').fadeOut(0);
+}
+
+function findItByClass(jQuerySelector){
+	return $(jQuerySelector).hasClass("flatironLink");
+}
+
+function formValue(){
+	return $('input:last').val();
+}
+
+
