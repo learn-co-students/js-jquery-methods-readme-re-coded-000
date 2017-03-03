1493d278a5c92e38279ce350469e7a3c1dee329e
rezanrezano <captenrez@gmail.com>
2017-03-04 01:11:32 +0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,27 @@
 'use strict';
+function fancySchmancy(){
+	$("#bacon").addClass("iAmFancy");
+	return $("#bacon");
+}
+function addText(string){
+	$('p:last').append(string);
+	return $('p:last');
+}
+function showDolphin(){
+	return $('#hidden').show();
+}
+function hideImage(){
+	return $("img[alt='pie in face']").hide();
+}
+function fadeIt(){
+	return $('#favorite-snacks').fadeIn();
+}
+function fadeItOut(){
+	return $('p:last').fadeOut(0);
+}
+function findItByClass(string){
+	return $(string).hasClass("flatironLink");
+}
+function formValue(){
+	return $('input:last').val();
+}
