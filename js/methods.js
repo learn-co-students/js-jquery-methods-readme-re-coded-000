d841fba4d9353c20beac860595def78ddcc0195a
dlkhwaz <dllaomer93@gmail.com>
2017-03-03 17:54:01 +0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,40 @@
 'use strict';
+function fancySchmancy(){
+	$('#bacon').addClass("iAmFancy");
+	return $(".iAmFancy");
+}
+function addText(string){
+	$('p:first-child + p').append(string);
+	return $('p:first-child + p');
+
+
+}
+function showDolphin(){
+	$('#hidden').show();
+
+}
+function hideImage(){
+	$('img[alt="pie in face"]').hide();
+}
+
+ function fadeIt(){
+ 	$('#favorite-snacks').fadeIn();
+ }
+
+ function fadeItOut(){
+ 	$('p:first-child + p').fadeOut(0);
+    return $('p:first-child + p');
+ }
+
+ function findItByClass(selector){
+ 	return $(selector).hasClass("flatironLink");
+ }
+ function formValue(){
+ 	return $('input:last').val();
+
+
+ }
+
+ function animateIt(){
+  $('#box').animate({height: "500px"});
+}
