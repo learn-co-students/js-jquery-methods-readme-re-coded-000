208df51eea007ad2909513b92833032bb5760181
Mustafa Taha <mtmax1994@gmail.com>
2017-03-05 11:11:18 +0300

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function fancySchmancy() {
+	$("#bacon").addClass("iAmFancy");
+	return $(".iAmFancy");
+}
+
+function addText(sentence){
+  $('p:first-child + p').append(sentence);
+  return $('p:first-child + p');
+
+}
+
+function showDolphin() {
+	$("img").show();
+}
+
+function hideImage() {
+	$("img").hide();
+}
+
+function fadeIt() {
+	$('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+	$("p").fadeOut(0)
+}
+
+function findItByClass(string) {
+	return $(string).hasClass("flatironLink")
+}
+
+function formValue() {
+	return $("input:last").val();
+}
+
+
